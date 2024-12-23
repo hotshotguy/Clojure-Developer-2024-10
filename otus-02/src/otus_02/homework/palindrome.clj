fee87e08c5bfd392bb30bd3f252ff11832334a8f
Ivan Shamshurin <shamshurin.iv@yandex.ru>
2024-12-23 14:50:28 +0300

    working palindrome
    added recur palindrome function

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,23 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
+(defn transform-string [s]
+  (-> s
+    (string/replace #"[,.-:;!?\"' ]" "")
+    (string/lower-case)))
 
-(defn is-palindrome [test-string])
+(defn recur-palindrom [test-vec]
+  (let [go-next (= (get test-vec 0) (get test-vec (dec (count test-vec))))]
+    (cond
+      (empty? test-vec) true
+      (== (count test-vec) 1) true
+      (not go-next) false
+      :else (recur-palindrom (subvec test-vec 1 (dec (count test-vec)))))))
 
+(defn is-palindrome [test-string]
+  (let [s (transform-string test-string)
+        s-vec (vec s)]
+    (recur-palindrom s-vec)))
+
+(comment
+  (is-palindrome "Civ, ic!"))
