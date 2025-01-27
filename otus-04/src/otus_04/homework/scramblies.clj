5cf8a6ff7ae6e09282e16942a16a53f640701a1d
Ivan Shamshurin <shamshurin.iv@yandex.ru>
2025-01-27 22:57:12 +0300

    scramblies

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -2,9 +2,18 @@
 
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+(defn is-gte? [coll [key value]]
+  (let [coll-value (coll key)]
+    (and coll-value (>= coll-value value))))
 
 (defn scramble?
   "Функция возвращает true, если из букв в строке letters
   можно составить слово word."
   [letters word]
-  nil)
+  (let [fword (frequencies word)
+        fletters (frequencies letters)]
+    (every? #(is-gte? fletters %) fword)))
+
+(comment
+  (scramble? "rkqodlw" "world")
+  )
